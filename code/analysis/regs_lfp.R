b5a6e7e1a6d86dfb02dd50c1f311028522e0783a
Christopher Severen <chris.severen@gmail.com>
2022-07-25 12:55:55 -0400

    all bound exercises

--- code/analysis/regs_lfp.R
+++ code/analysis/regs_lfp.R
@@ -14,6 +14,12 @@ library(broom)  # v 0.7.4
 library(tidyverse)  # v 1.3.0 
 library(data.table)
 
+myDict = c("d_black::1:year_bin::1980" = "$1[\\text{Black}] \\times t_{1980}$",
+           "d_black::1:year_bin::1990" = "$1[\\text{Black}] \\times t_{1990}$",
+           "d_black::1:year_bin::2000" = "$1[\\text{Black}] \\times t_{2000}$",
+           "d_black::1:year_bin::2010" = "$1[\\text{Black}] \\times t_{2005--11}$",
+           "d_black::1:year_bin::2019" = "$1[\\text{Black}] \\times t_{2012--19}$")
+
 ##########################################
 
 ipums <- fread("./empirics/data/ipums_smaller_lfp.csv")
@@ -22,6 +28,8 @@ ipums <- fread("./empirics/data/ipums_smaller_lfp.csv")
 ## the error: "Error in feols(...  : 
 ##             Error : cannot allocate vector of size 3.2 Gb"
 ## If this occurs, simply wait a short period of time and run the error-generating model. It should execute.
+
+## Main (full sample) LFP robustness
 
 RB_0.1 <- feols(ln_trantime ~ i(d_black,i.year_bin, 0) | year_bin,
                ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
@@ -67,5 +75,122 @@ etable(RB_0.2, RB_A.2, RB_C.2, keep = "d_black", digits=3)
 etable(RB_0.3, RB_A.3, RB_C.3, keep = "d_black", digits=3)
 
 etable(RB_0.1, RB_A.1, RB_C.1, RB_0.2, RB_A.2, RB_C.2, RB_0.3, RB_A.3, RB_C.3, 
-       keep = "d_black", tex=TRUE, digits=3, replace=TRUE,
+       keep = "t_", tex=TRUE, digits=3, replace=TRUE, dict = myDict,
        file = paste0(gdir,"\\results\\black-white\\tables\\bounds_lfpr.tex") )
+
+
+## Prime age LFP (all)
+
+PA_0.1 <- feols(ln_trantime ~ i(d_black,i.year_bin, 0) | year_bin,
+                subset = (ipums$age>=25 & ipums$age<=54), 
+                ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+
+PA_0.2 <- feols(ln_trantime ~ i(d_black,i.year_bin, 0) | czone_year_bin, 
+                subset = (ipums$age>=25 & ipums$age<=54), 
+                ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+PA_0.3 <- feols(ln_trantime ~ i(d_black,i.year_bin, 0) | czone_year_bin + 
+                  year_bin^educ_bin +
+                  year_bin[female, age, age2, d_marr, d_head, child_1or2, child_gteq3],
+                subset = (ipums$age>=25 & ipums$age<=54), 
+                ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+
+PA_C.1 <- feols(ln_trantime_q99 ~ i(d_black,i.year_bin, 0) | year_bin,
+                subset = (ipums$age>=25 & ipums$age<=54), 
+                ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+
+PA_C.2 <- feols(ln_trantime_q99 ~ i(d_black,i.year_bin, 0) | czone_year_bin, 
+                subset = (ipums$age>=25 & ipums$age<=54), 
+                ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+PA_C.3 <- feols(ln_trantime_q99 ~ i(d_black,i.year_bin, 0) | czone_year_bin + 
+                  year_bin^educ_bin +
+                  year_bin[female, age, age2, d_marr, d_head, child_1or2, child_gteq3],
+                subset = (ipums$age>=25 & ipums$age<=54), 
+                ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+           
+etable(PA_0.1, PA_C.1, PA_0.2, PA_C.2, PA_0.3, PA_C.3, 
+       keep = "t_", tex=TRUE, digits=3, replace=TRUE, dict = myDict,
+       file = paste0(gdir,"\\results\\black-white\\tables\\bounds_lfpr_primeage.tex") )
+
+
+## Prime age LFP (male)
+
+PA_0.1m <- feols(ln_trantime ~ i(d_black,i.year_bin, 0) | year_bin,
+                subset = (ipums$age>=25 & ipums$age<=54 & ipums$female==0), 
+                ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+
+PA_0.2m <- feols(ln_trantime ~ i(d_black,i.year_bin, 0) | czone_year_bin, 
+                subset = (ipums$age>=25 & ipums$age<=54 & ipums$female==0), 
+                ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+PA_0.3m <- feols(ln_trantime ~ i(d_black,i.year_bin, 0) | czone_year_bin + 
+                  year_bin^educ_bin +
+                  year_bin[age, age2, d_marr, d_head, child_1or2, child_gteq3],
+                subset = (ipums$age>=25 & ipums$age<=54 & ipums$female==0), 
+                ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+
+PA_C.1m <- feols(ln_trantime_q99 ~ i(d_black,i.year_bin, 0) | year_bin,
+                subset = (ipums$age>=25 & ipums$age<=54 & ipums$female==0), 
+                ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+
+PA_C.2m <- feols(ln_trantime_q99 ~ i(d_black,i.year_bin, 0) | czone_year_bin, 
+                subset = (ipums$age>=25 & ipums$age<=54 & ipums$female==0), 
+                ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+PA_C.3m <- feols(ln_trantime_q99 ~ i(d_black,i.year_bin, 0) | czone_year_bin + 
+                  year_bin^educ_bin +
+                  year_bin[age, age2, d_marr, d_head, child_1or2, child_gteq3],
+                subset = (ipums$age>=25 & ipums$age<=54 & ipums$female==0), 
+                ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+
+etable(PA_0.1m, PA_C.1m, PA_0.2m, PA_C.2m, PA_0.3m, PA_C.3m, 
+       keep = "t_", tex=TRUE, digits=3, replace=TRUE, dict = myDict,
+       file = paste0(gdir,"\\results\\black-white\\tables\\bounds_lfpr_primeage_male.tex") )
+
+
+## Prime age LFP (female)
+
+PA_0.1f <- feols(ln_trantime ~ i(d_black,i.year_bin, 0) | year_bin,
+                 subset = (ipums$age>=25 & ipums$age<=54 & ipums$female==1), 
+                 ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+
+PA_0.2f <- feols(ln_trantime ~ i(d_black,i.year_bin, 0) | czone_year_bin, 
+                 subset = (ipums$age>=25 & ipums$age<=54 & ipums$female==1), 
+                 ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+PA_0.3f <- feols(ln_trantime ~ i(d_black,i.year_bin, 0) | czone_year_bin + 
+                   year_bin^educ_bin +
+                   year_bin[age, age2, d_marr, d_head, child_1or2, child_gteq3],
+                 subset = (ipums$age>=25 & ipums$age<=54 & ipums$female==1), 
+                 ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+
+PA_C.1f <- feols(ln_trantime_q99 ~ i(d_black,i.year_bin, 0) | year_bin,
+                 subset = (ipums$age>=25 & ipums$age<=54 & ipums$female==1), 
+                 ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+
+PA_C.2f <- feols(ln_trantime_q99 ~ i(d_black,i.year_bin, 0) | czone_year_bin, 
+                 subset = (ipums$age>=25 & ipums$age<=54 & ipums$female==1), 
+                 ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+PA_C.3f <- feols(ln_trantime_q99 ~ i(d_black,i.year_bin, 0) | czone_year_bin + 
+                   year_bin^educ_bin +
+                   year_bin[age, age2, d_marr, d_head, child_1or2, child_gteq3],
+                 subset = (ipums$age>=25 & ipums$age<=54 & ipums$female==1), 
+                 ipums, cluster = "czone", weights = ipums$czwt_tt, lean = TRUE, mem.clean = TRUE)
+
+
+etable(PA_0.1f, PA_C.1f, PA_0.2f, PA_C.2f, PA_0.3f, PA_C.3f, 
+       keep = "t_", tex=TRUE, digits=3, replace=TRUE, dict = myDict,
+       file = paste0(gdir,"\\results\\black-white\\tables\\bounds_lfpr_primeage_female.tex") )
